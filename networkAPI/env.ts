1e217feb053c8a6d7789a2b235beaa71c9760c82
Shailendra singh <ssin09559@gmail.com>
2023-08-20 13:14:29 +0530

    changes

--- networkAPI/env.ts
+++ networkAPI/env.ts
@@ -1,3 +1,7 @@
-export const API_URL = "http://localhost:12002/api";
 
+export const API_URL = "https://api.elaundry.co.in/oit-elaundry/api/";
+
+
+
+// export const API_URL = "http://localhost:12002/api";
 // 192.168.137.1
