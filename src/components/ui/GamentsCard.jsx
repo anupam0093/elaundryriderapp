7a87c938c2820fe40c0d694aabc6cb8c68aa7de9
Nehat <nehat@Nehats-MacBook-Pro.local>
2023-09-27 17:59:43 +0530

    added image url path to item

--- src/components/ui/GamentsCard.jsx
+++ src/components/ui/GamentsCard.jsx
@@ -3,10 +3,11 @@ import { MaterialIcons } from '@expo/vector-icons';
 import React from 'react'
 
 const GamentsCard = ({setShowModal, item, openModal}) => {
+  const IMAGE_BASE_URL = 'https://nehat.pythonanywhere.com/media/garment-images/'
   return (
     <View style={{ flexDirection: 'row', marginTop: 10, marginHorizontal: 15, paddingVertical: 10, paddingHorizontal: 10, backgroundColor: 'white', alignItems: 'center', justifyContent: 'space-between', elevation: 10, borderRadius: 5 }}>
     <View style={{ flexDirection: 'row', alignItems: 'center', gap: 20 }}>
-      <Image source={{ uri: 'https://gildan.my/wp-content/uploads/2020/02/76000B-24C-Gold.png' }} style={{ width: 60, height: 60, resizeMode: 'contain' }} />
+      <Image source={{ uri: `${IMAGE_BASE_URL}${item?.garmentImagePath}` }} style={{ width: 60, height: 60, resizeMode: 'contain' }} />
       <View style={{ gap: 10, width:'60%' }}>
         <Text style={{ fontSize: 16, fontWeight: 600 }} numberOfLines={3} adjustsFontSizeToFit={true}>{item?.garmentName}</Text>
         <Text style={{ fontSize: 16, fontWeight: 400 }}>{'\u20B9'} {item?.price}</Text>
